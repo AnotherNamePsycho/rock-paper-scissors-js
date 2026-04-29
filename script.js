2f8d18abeb5d4e3865e5dec30535f78c4846fb46
nawin <nawin.ong2007@gmail.com>
2026-04-29 19:54:38 +0700

    Add getComputerChoice to generate move of computer

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello ,World!")
+//Randomly generate number between 0 to 2 and return the move choice according to number
+//0 = rock || 1 = paper || 2 = scissors 
+function getComputerChoice(){
+    const choice = Math.floor(Math.random() * 3);
+
+    if(choice == 0){
+        return 'rock';
+    }
+    else if(choice == 1){
+        return 'paper';
+    }
+    else if(choice == 2){
+        return 'scissors';
+    }
+}
