304c4f195504fbdf6ff0762f0c0c167da9643e61
nawin <nawin.ong2007@gmail.com>
2026-05-01 19:42:12 +0700

    Implement logic for a single game round
    - Initialize the score for player and computer
    - Add function playRound that play a single round

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ function getComputerChoice(){
 function getHumanChoice(){
     return prompt('Input your choice');
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+//get input from player and generate computer move
+//show who win the round and add one point to their score
+function playRound(humanChoice,computerChoice){
+    const _computerChoice = computerChoice();
+    const _humanChoice = humanChoice().toLowerCase();
+
+    let isWin = false;
+
+    if(_humanChoice == 'rock' && _computerChoice == 'scissors'){
+        isWin = true;
+    }
+    else if (_humanChoice == 'paper' && _computerChoice == 'rock'){
+        isWin = true;
+    }
+    else if (_humanChoice == 'scissors' && _computerChoice == 'paper'){
+        isWin = true;
+    }
+
+    if(isWin && _humanChoice == _computerChoice){
+        console.log("Tie!");
+    }
+    else if(isWin){
+        console.log(`You win! ${_humanChoice} beats ${_computerChoice}`);
+        humanScore++;
+    }
+    else{
+        console.log(`You lose! ${_computerChoice} beats ${_humanChoice}`);
+        computerScore++;
+    }
+
+}
+
