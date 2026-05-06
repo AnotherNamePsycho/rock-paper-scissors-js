04bd83dfdff4d42a102365160b9cd84ed85935fe
nawin <nawin.ong2007@gmail.com>
2026-05-07 01:40:20 +0700

    Remove old gameloop and Implement choice button
    - Remove 5 rounds game because we change the rule
      to first 5 points win

--- script.js
+++ script.js
@@ -14,65 +14,46 @@ function getComputerChoice(){
     }
 }
 
-// return player input string
-function getHumanChoice(){
-    return prompt('Input your choice');
-}
+let humanScore = 0;
+let computerScore = 0;
 
-//play game for five round and show the winner
-function playGame(){
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    //show who win the round and add one point to their score
-    function playRound(humanChoice,computerChoice){
-        humanChoice = humanChoice.toLowerCase();
-        let isWin = false;
-
-        if(humanChoice == 'rock' && computerChoice == 'scissors'){
-            isWin = true;
-        }
-        else if (humanChoice == 'paper' && computerChoice == 'rock'){
-            isWin = true;
-        }
-        else if (humanChoice == 'scissors' && computerChoice == 'paper'){
-            isWin = true;
-        }
-
-    
-        if(isWin){
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-            humanScore++;
-        }
-        else if(!isWin && humanChoice == computerChoice){
-            console.log(`Tie! You both pick ${humanChoice}.`);
-        }
-        else{
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-            computerScore++;
-        }
-
+//find winner and add score
+function playRound(humanChoice,computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    let isWin = false;
+    if(humanChoice == 'rock' && computerChoice == 'scissors'){
+        isWin = true;
+    }
+    else if (humanChoice == 'paper' && computerChoice == 'rock'){
+        isWin = true;
+    }
+    else if (humanChoice == 'scissors' && computerChoice == 'paper'){
+        isWin = true;
     }
 
-
-    
-    for(let i = 0; i < 5;i++){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice,computerChoice);
+    if(isWin){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
     }
-
-    if(humanScore > computerScore){
-        console.log('Human win!');
-    }
-    else if(humanScore < computerScore){
-        console.log('Computer win!');
+    else if(!isWin && humanChoice == computerChoice){
+        console.log(`Tie! You both pick ${humanChoice}.`);
     }
     else{
-        console.log('Tie!');
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
     }
-    console.log(`Score-> Human: ${humanScore} | Computer: ${computerScore}`);
 }
 
-playGame()
+
+function initButtonListener(){
+    let buttonList = Array.from(document.querySelectorAll("button"));
+
+    buttonList.forEach((button) => {
+        button.addEventListener("click",(e) => {
+            playRound(e.target.textContent,getComputerChoice())
+            
+        })
+    });
+}
+
+initButtonListener();
