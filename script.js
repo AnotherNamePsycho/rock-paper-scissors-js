900dab98ac0862c5248cf8e829cf406719797a1d
nawin <nawin.ong2007@gmail.com>
2026-05-07 18:43:05 +0700

    Implement dynamic ui update using event

--- script.js
+++ script.js
@@ -17,8 +17,12 @@ function getComputerChoice(){
 let humanScore = 0;
 let computerScore = 0;
 
+let humanChoice;
+let computerChoice;
+
 //find winner and add score
 function playRound(humanChoice,computerChoice){
+
     humanChoice = humanChoice.toLowerCase();
     let isWin = false;
     if(humanChoice == 'rock' && computerChoice == 'scissors'){
@@ -42,6 +46,9 @@ function playRound(humanChoice,computerChoice){
         console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
         computerScore++;
     }
+    
+    let event = new Event("round-end");
+    document.dispatchEvent(event);
 }
 
 
@@ -50,33 +57,73 @@ function initButtonListener(){
 
     buttonList.forEach((button) => {
         button.addEventListener("click",(e) => {
-            playRound(e.target.textContent,getComputerChoice())
-            
+            humanChoice = e.target.textContent;
+            computerChoice = getComputerChoice();
+            playRound(humanChoice,computerChoice);
         })
     });
 }
 
+
 function initRoundInfoElement(){
     const roundInfo = document.querySelector("#round-info");
     const info = document.createElement("p");
-    info.textContent = `info:`;
-    roundInfo.appendChild(info);
+    
+    const humanChoiceText = document.createElement("p");
+    humanChoiceText.classList.add("human-color")
+    humanChoiceText.textContent = `Human: `;
+    roundInfo.appendChild(humanChoiceText);
+
+    const computerChoiceText = document.createElement("p");
+    computerChoiceText.classList.add("computer-color")
+    computerChoiceText.textContent = `Computer: `;
+    roundInfo.appendChild(computerChoiceText);
+
+    const roundWinner = document.createElement("p");
+    roundWinner.id = "round-winner";
+    roundInfo.appendChild(roundWinner);
 }
 
 function initScoreCounterElement(){
     const scoreCounter = document.querySelector("#score-counter");
-    
+
     const humanScoreText = document.createElement("p");
-    humanScoreText.classList.add("human-score")
+    humanScoreText.classList.add("human-color")
     humanScoreText.textContent = `Human: ${humanScore}`;
     scoreCounter.appendChild(humanScoreText);   
 
     const computerScoreText = document.createElement("p");
-    computerScoreText.classList.add("computer-score")
+    computerScoreText.classList.add("computer-color")
     computerScoreText.textContent = `Computer: ${computerScore}`;
     scoreCounter.appendChild(computerScoreText);
 }
 
-initButtonListener();
-initRoundInfoElement();
-initScoreCounterElement();
+
+function updateUI(){
+    const humanInfo = document.querySelector("#round-info .human-color");
+    const computerInfo = document.querySelector("#round-info .computer-color");
+    const roundWinner = document.querySelector("#round-winner");
+
+    humanInfo.textContent = `Human: ${humanChoice}`;
+    computerInfo.textContent = `Computer: ${computerChoice}`;
+    
+
+    const humanScoreText = document.querySelector("#score-counter .human-color");
+    const computerScoreText = document.querySelector("#score-counter .computer-color");
+    
+    humanScoreText.textContent = `Human: ${humanScore}`;
+    computerScoreText.textContent = `Computer: ${computerScore}`;
+}
+
+
+function init(){
+    initButtonListener();
+    initRoundInfoElement();
+    initScoreCounterElement();
+    
+    document.addEventListener("round-end",() => {
+        updateUI();
+    });
+}
+
+init();
